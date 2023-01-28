99bcdc6dbc6314058f977085bdc7105e298e7f66
altajvirani <altajvirani786@gmail.com>
2023-01-28 18:45:43 +0530

    before merging with bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Container from '.';
-import Row from './react-bootstrap/Row';
-import Col from './react-bootstrap/Col';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Container>
-      <Row>
-        <Col style={{backgroundColor: "red"}}>1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
-    </Container>
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
