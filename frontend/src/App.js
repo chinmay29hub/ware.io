7d750d9c19166cba651dba4b60b2656003bf255c
chinmay29hub <chinmaysonawane57@gmail.com>
2023-01-28 18:39:46 +0530

    fixed react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+// import './App.css';
+import Container from 'react-bootstrap/Row';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    // <div className="App">
+    
+    // </div>
+    <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
   );
 }
 
